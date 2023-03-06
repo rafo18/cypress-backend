cf96c9ec9c88a1169b43bcc7189eae115c8e3750
rafo18 <rafaelbarreto10@hotmail.com>
2023-03-06 17:40:03 -0400

    probando el body

---
+++ cypress/e2e/backend/body.cy.js
@@ -0,0 +1,17 @@
+describe('probando el body', () => {
+    it('probar el body2', () => {
+        cy.request('/employees/1')
+        .its('body')
+        .its('first_name')
+        .should('be.equal', 'Javier')
+
+        cy.request('/employees/1')
+        .then(response => {
+            expect(response.status).to.be.equal(200)
+            expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8')
+            expect(response.body.first_name).to.be.equal('Javier')
+            expect(response.body.last_name).to.be.equal('Eschweiler')
+        })
+
+    });
+});
